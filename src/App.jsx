ec5ff01bf757ab1b6a701c348942edcb2837df43
“sarthak-gupta229Git config --global user.email “guptasarthak229@gmail.com <guptasarthak229@gmail.com>
2026-01-24 02:09:15 +0530

    started project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-3xl font-bold text-violet-300'>My name is Sarthak</h1>
+    </main>
+  )
+}
+
+export default App
